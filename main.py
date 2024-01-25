5445599b51640c039445f81890a8252b24a897f9
Iskan09 <152530058+Iskan09@users.noreply.github.com>
2024-01-25 19:38:06 +0300

    1

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from random import randint
 from flask import Flask, render_template
 from datetime import datetime
+from datetime import date
 app = Flask(__name__)
 
 @app.route('/')
 def index() -> str:
-    return render_template("index.html", name="Nikita")
+
+    return render_template("index.html", name="Nikita",)
 
 @app.route('/user/<username>/<age>')
-def user_info(username, age):
+def user_info(username: str, age: str) -> str:
     info = (f'<h1>Hi {username}</h1><br>'
             f'<p>You are {age} years old<p>')
     return info
 @app.route('/time/')
 def time() -> str:
-    return render_template("time.html", now=datetime.now().time())
+    now = datetime.now()
+    current_date = str(date.today())[6:7]
+    current_date = int(current_date)
+    if current_date <= 6:
+        mouths_to_summer = 6 - current_date
+    elif current_date > 6 and current_date < 9:
+        mouths_to_summer = 0
+    else:
+        mouths_to_summer = current_date - 6
+    print(mouths_to_summer)
+    return render_template("time.html", now=now.strftime("%H:%M:%S"), mouths=mouths_to_summer)
     
 @app.route('/random/')
 def random() -> str:
-    return render_template("random.html", num=randint(0, 5))
+    return render_template("random.html", num=randint(0, 99))
 
 app.run(debug=True)
